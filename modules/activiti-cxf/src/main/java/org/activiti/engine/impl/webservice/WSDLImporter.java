996a1f864e3ffef334e2c5e3405dd7ad45989838
cdeneux <christophe.deneux@gmail.com>
2014-12-09 10:29:55 +0100

    Rework XML parsing of WSDLImporter using the J2SE standard way instead of using directly Xerces classes. Caution: this commit remove the dependency on Xerces, the DOM parser of the J2SE will be used. If Xerces is required, just re-add it as dependency in the POM.

--- modules/activiti-cxf/src/main/java/org/activiti/engine/impl/webservice/WSDLImporter.java
+++ modules/activiti-cxf/src/main/java/org/activiti/engine/impl/webservice/WSDLImporter.java
@@ -31,6 +31,12 @@ import javax.wsdl.extensions.soap.SOAPAddress;
 import javax.wsdl.factory.WSDLFactory;
 import javax.wsdl.xml.WSDLReader;
 import javax.xml.namespace.QName;
+<<<<<<< HEAD
+=======
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+>>>>>>> Rework XML parsing of WSDLImporter using the J2SE standard way instead
 
 import org.activiti.bpmn.model.Import;
 import org.activiti.engine.ActivitiException;
@@ -222,15 +228,16 @@ public class WSDLImporter implements XMLImporter {
 
   private Element getRootTypes() {
     try {
-      DOMParser parser = new DOMParser();
-      parser.parse(this.wsdlLocation);
-      Document doc = parser.getDocument();
+      DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+      Document doc = docBuilder.parse(this.wsdlLocation);
       Element root = (Element) doc.getFirstChild();
       Element typesElement = (Element) root.getElementsByTagName("wsdl:types").item(0);
       return (Element) typesElement.getElementsByTagNameNS("http://www.w3.org/2001/XMLSchema", "schema").item(0);
     } catch (SAXException e) {
       throw new ActivitiException(e.getMessage(), e);
     } catch (IOException e) {
+      throw new ActivitiException(e.getMessage(), e);
+    } catch (ParserConfigurationException e) {
       throw new ActivitiException(e.getMessage(), e);
     }
   }
